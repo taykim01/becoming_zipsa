783bc7c15ab27575973dbe72778a5a98b26ca2b9
jjuying <dendy01@naver.com>
2024-07-02 20:46:13 +0900

    image url

--- src/lib/images/index.ts
+++ src/lib/images/index.ts
@@ -1,11 +1,12 @@
 const Images = {
-    tub: "",
-    shampoo: "",
-    paw: "",
-    bin: "",
-    tablet: "",
-    kit: "",
-    toy: "",
+    bath:"https://firebasestorage.googleapis.com/v0/b/stardenburdenhardenbart-kairos.appspot.com/o/website_images%2Fbath.webp?alt=media&token=04ff78da-ee6b-4b74-b35a-b872eab5d21b",
+    house:"https://firebasestorage.googleapis.com/v0/b/stardenburdenhardenbart-kairos.appspot.com/o/website_images%2Fhouse.webp?alt=media&token=97b47792-efd2-4740-977e-a3ea64d42260",
+    paw: "https://firebasestorage.googleapis.com/v0/b/stardenburdenhardenbart-kairos.appspot.com/o/website_images%2Fpaw.webp?alt=media&token=8ae49c42-9e0d-451a-bd84-3a4a2f4dede1",
+    bin: "https://firebasestorage.googleapis.com/v0/b/stardenburdenhardenbart-kairos.appspot.com/o/website_images%2Fbin.webp?alt=media&token=6f25aef3-a773-4942-a1bf-77c70017370c",
+    tablet: "https://firebasestorage.googleapis.com/v0/b/stardenburdenhardenbart-kairos.appspot.com/o/website_images%2Ftablet.webp?alt=media&token=571b0038-8b4f-4c21-a6e5-7b3cdf4dfbba",
+    kit: "https://firebasestorage.googleapis.com/v0/b/stardenburdenhardenbart-kairos.appspot.com/o/website_images%2Fkit.webp?alt=media&token=a5c82941-2439-4c4e-b85b-18670669977a",
+    toy: "https://console.firebase.google.com/u/0/project/stardenburdenhardenbart-kairos/storage/stardenburdenhardenbart-kairos.appspot.com/files/~2Fwebsite_images",
+    heart:"https://firebasestorage.googleapis.com/v0/b/stardenburdenhardenbart-kairos.appspot.com/o/website_images%2Fheart.webp?alt=media&token=d1c88e1a-925b-45e5-9688-290682612f65",
 }
 
 export default Images;
