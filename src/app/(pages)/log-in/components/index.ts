2ef66b9e9c5bb435d04dbb96b3ea157b52d7e5c7
taykim01 <taykim01@gmail.com>
2024-07-04 00:02:42 +0900

    fix: merge errors

--- src/app/(pages)/log-in/components/index.ts
+++ src/app/(pages)/log-in/components/index.ts
@@ -1,18 +1,11 @@
 import InputLogIn from "./input_log_in";
 import SignUpButton from "./sign_up_button";
-<<<<<<< HEAD
-
-const Components = {
-    SignUpButton,
-    InputLogIn
-=======
 import SignUpPopup from "./sign_up_popup";
 
 const Components = {
     SignUpButton,
     InputLogIn,
     SignUpPopup
->>>>>>> 65d9cea4ef3a1eb003596bf3d876b56153504e0c
 }
 
 export default Components;
