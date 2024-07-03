e2cee8fde07468f1d5d5883bf63198d0e147f189
flging <122280439+flging@users.noreply.github.com>
2024-07-04 00:06:11 +0900

    feat: 병합

--- src/app/(pages)/log-in/components/sign_up_button.tsx
+++ src/app/(pages)/log-in/components/sign_up_button.tsx
@@ -1,18 +1,5 @@
 "use client"
 
-<<<<<<< HEAD
-import { useRouter } from "next/navigation";
-
-export default function SignUpButton() {
-    const router = useRouter();
-    return (
-        <div
-            className="font-fs-r text-gray-dark text-r16"
-            onClick={() => router.push("/sign-up")}
-        >
-            회원가입하기
-        </div>
-=======
 import Components from ".";
 import { useState } from "react";
 
@@ -28,6 +15,5 @@ export default function SignUpButton() {
         </div>
         <Components.SignUpPopup open={popup} onClose={() => setPopup(false)} />
         </>
->>>>>>> 65d9cea4ef3a1eb003596bf3d876b56153504e0c
     )
 }
--- src/app/(pages)/sign-up/components/index.ts
+++
@@ -1,7 +0,0 @@
-import InputSignUp from "./input_sign_up";
-
-const Components = {
-    InputSignUp
-}
-
-export default Components;
