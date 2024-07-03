8e1286ef2019162866eec888903eeb591b0c4fd4
flging <122280439+flging@users.noreply.github.com>
2024-07-03 16:06:33 +0900

    create badge

--- src/app/(pages)/my-cat/badge/page.tsx
+++ src/app/(pages)/my-cat/badge/page.tsx
@@ -1,5 +1,23 @@
-export default function Page() {
+import Container from "@/lib/container";
+import Image from "next/image";
+import Components from "./components";
+
+export default function Badge() {
     return (
-        <></>
+        <Container.Main headerTitle="내 고양이">
+            <div className="flex flex-col items-center justify-between relative w-full h-full gap-8">
+                <div className="flex flex-col items-center w-full gap-11 relative">
+                    <Image
+                        src="https://firebasestorage.googleapis.com/v0/b/stardenburdenhardenbart-kairos.appspot.com/o/website_images%2Fsample_pet.png?alt=media&token=0dc2e7c4-d7f0-4365-b0e9-f0deedc88735"
+                        alt="내 고양이"
+                        width={195}
+                        height={195}
+                    />
+                    <Components.CatInfo />
+                </div>
+
+                <Components.InteractionGroup />
+            </div>
+        </Container.Main>
     )
 }
