d0d91c3f88a475aa3f67282c0cf0a8fa3f231a84
taykim01 <taykim01@gmail.com>
2024-07-03 16:11:54 +0900

    fix auth usecases

--- src/domain/use_case/auth/check_session_use_case.ts
+++ src/domain/use_case/auth/check_session_use_case.ts
@@ -1,1 +1,13 @@
-export default class CheckAuthSessionUseCase {}
+import MyResponse from "../MyResponse";
+
+export default class CheckAuthSessionUseCase {
+    async check():Promise<MyResponse>{
+        try {
+            const cachedData = sessionStorage.getItem('scholarshipListModel')
+            if(!cachedData) return new MyResponse(false, "세션이 없습니다.", {})
+            return new MyResponse(true, "인증에 성공했습니다.", cachedData)
+        } catch (error) {
+            return new MyResponse(false, "인증에 실패했습니다.", String(error))
+        }
+    }
+}
--- src/domain/use_case/auth/sign_up_use_case.ts
+++ src/domain/use_case/auth/sign_up_use_case.ts
@@ -3,24 +3,31 @@ import MyResponse from "../MyResponse"
 import { auth, provider } from "@/firebase"
 
 export default class SignUpUseCase {
-    // sign-up api 호출
     async signUp(): Promise<MyResponse> {
         try {
             const response = await signInWithPopup(auth, provider);
             const credential = GoogleAuthProvider.credentialFromResult(response);
             if (!credential) return new MyResponse(true, "이미 인증된 사용자", response.user)
+            const uid = response.user.uid
+            sessionStorage.setItem('uid', JSON.stringify(uid));
             return new MyResponse(true, "인증에 성공했습니다.", response.user)
         } catch (error) {
             return new MyResponse(false, "인증에 실패했습니다.", String(error))
         }
     }
 
-    // user 정보를 db에 저장
-    async createUser(): Promise<MyResponse> {
+    async createUser(userData: User): Promise<MyResponse> {
         try {
-
-
-            return new MyResponse(true, "유저 데이터 생성에 성공했습니다.", {})
+            const res = await fetch(`https://localhost/api/v1/user/create`, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(userData)
+            })
+            const data = await res.json()
+            if (!data.success) return new MyResponse(false, "유저 데이터 생성에 실패했습니다.", {})
+            return new MyResponse(true, "유저 데이터 생성에 성공했습니다.", data.data)
         } catch (error) {
             return new MyResponse(false, "유저 데이터 생성에 실패했습니다.", String(error))
         }
