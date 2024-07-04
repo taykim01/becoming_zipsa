c098715b007fc00ae575d3c7e3fdb45ab98c9bca
taykim01 <taykim01@gmail.com>
2024-07-04 19:29:22 +0900

    fix: myUrl

--- src/domain/my_url.ts
+++ src/domain/my_url.ts
@@ -1,6 +1,6 @@
 const myUrl =
     process.env.NEXT_PUBLIC_ENVIRONMENT === "production"
-        ? "https://stardenburdenhardenbart.vercel.app/"
-        : "http://localhost:3000/";
+        ? "https://stardenburdenhardenbart.vercel.app"
+        : "http://localhost:3000";
 
 export default myUrl;
