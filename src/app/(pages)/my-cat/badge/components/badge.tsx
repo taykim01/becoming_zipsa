8265466dd5ba7101e1e1f8da5be823bd555f791f
taykim01 <taykim01@gmail.com>
2024-07-04 15:02:33 +0900

    fix: build error fix

--- src/app/(pages)/my-cat/badge/components/badge.tsx
+++ src/app/(pages)/my-cat/badge/components/badge.tsx
@@ -1,11 +1,9 @@
 import Badge from "@/lib/badge/badge"
 
 export default function MyCatBadge(props: {
-    badgeTitle:string,
-    // get?:boolean
-}){
-    return(
+    badgeTitle: "깨끗한 고양이" | "건강한 고양이" | "씩씩한 고양이",
+}) {
+    return (
         <Badge title={props.badgeTitle}></Badge>
     )
-    //{get && <Badge title={props.badgeTitle}></Badge>}
 }
