54ea08dba0ebc623e9853fee5c4f004382baf0bd
taykim01 <taykim01@gmail.com>
2024-07-17 01:43:23 +0900

    test console

---
+++ src/app/(pages)/test/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+import React from "react"
+import URL from "../../../repository/url"
+
+export default function Page() {
+    console.log(URL, process.env.NEXT_PUBLIC_ENVIRONMENT)
+    return<></>
+}
