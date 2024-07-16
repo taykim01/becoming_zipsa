03cfbdb86470edca6afe8188f5cccd98dc5244f1
taykim01 <taykim01@gmail.com>
2024-07-16 21:54:32 +0900

    production: v1.0.5.B

--- src/utils/check_session_tag.tsx
+++ src/utils/check_session_tag.tsx
@@ -2,8 +2,9 @@
 
 import { useLoadingRouter } from "@/hooks/use_loading_router"
 import { loadingState } from "@/recoil/loading"
+import ReadCat from "@/repository/v1.0.0/cat/read_cat"
 import CheckSession from "@/repository/v1.0.0/user/check_session"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useSetRecoilState } from "recoil"
 
 
@@ -13,11 +14,14 @@ export default function CheckSessionTag(props: {
     googleException?: boolean
 }) {
     const check_session = new CheckSession()
+    const read_cat = new ReadCat()
 
 
     const router = useLoadingRouter()
     const setLoading = useSetRecoilState(loadingState)
-    const catData = localStorage.getItem("catData") || false
+    const readCatData = () => read_cat.read()
+    const [isCat, setIsCat] = useState(false)
+
     
     const checkSession = async () => {
         setLoading(true)
@@ -25,13 +29,19 @@ export default function CheckSessionTag(props: {
         if (!response.success && props.noSession) router(props.noSession)
         if (response.success && props.session) {
             if (props.googleException) {
-                if (catData) router(props.session)
+                if (isCat) router(props.session)
             } else router(props.session)
         }
         setLoading(false)
     }
+
+    
     useEffect(() => {
-        checkSession()
+        readCatData().then((response) => {
+            setIsCat(response.success)
+        }).finally(() => {
+            checkSession()
+        })
     }, [])
     return <></>
 }
